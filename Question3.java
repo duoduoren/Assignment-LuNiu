7a8a63e53901c0e73872ac33dd55bbc5d6ef77fe
duoduoren <31637333+duoduoren@users.noreply.github.com>
2017-10-07 14:42:39 -0700

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,57 @@
+package assignment4;
+
+class IpAddress {
+	int[] octets;
+	String dottedDecimal;
+	String IpAddress;
+	
+	// Suppose ip is well formatted dotted-decimal ip.
+	public IpAddress(String ip) {
+		this.IpAddress = ip;
+		
+		// This will return an string array of octet,
+		// With the array length 4.
+		String[] octets = ip.split(".");
+
+		// Initializes array before using it.
+		this.octets = new int[4];
+		
+		// Put each octet to an Array.
+		this.octets[0] = Integer.parseInt(octets[0]);
+		this.octets[1] = Integer.parseInt(octets[1]);
+		this.octets[2] = Integer.parseInt(octets[2]);
+		this.octets[3] = Integer.parseInt(octets[3]);
+		this.IpAddress = ip;
+//	    	for(int i = 0; i < ip.length(); i++) {
+//	    		if (ip.charAt(i) == '.' ) {
+//	    			Octeti = ip.substring(0, i-1);
+//	    		}
+//	    		
+//	    		StrOcteti = IpAdress.substring(0, i);
+//	    	}
+	}
+	
+	public void setDottedDecimal(String IpAdress) {
+		this.IpAddress = IpAdress;
+	}
+
+	public String getDottedDecimal() {
+	    	return this.IpAddress; 	
+	}
+	
+	// Gets the octet according to the position.
+	public int getOctet(int position) {
+		return this.octets[position];
+	}
+
+
+	static void main(String[] args){
+		IpAddress ip = new IpAddress("216.27.6.136");
+		System.out.println(ip.getDottedDecimal());
+		System.out.println(ip.getOctet(4));
+		System.out.println(ip.getOctet(1));
+		System.out.println(ip.getOctet(3));
+		System.out.println(ip.getOctet(2));
+	}
+}
+
